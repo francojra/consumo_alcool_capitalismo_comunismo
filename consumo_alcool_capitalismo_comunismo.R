5728fa1505f797e24b1044e48d0a8d8020aa9d67
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-05 21:03:23 -0300

    Add files via upload

---
+++ consumo_alcool_capitalismo_comunismo.R
@@ -0,0 +1,32 @@
+
+# Consumo de álcool em países capitalistas e comunistas ------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 05/10/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/alcohol-consumption -------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### O álcool tem historicamente, e continua a ter, um papel importante no engajamento
+### social e criação de laços para muitos. Beber álcool socialmente e moderadamente
+### para muitos é prazeroso.
+
+### Entretanto, o consumo do álcool - principalmente em excesso - é ligado a um numeroso
+### resultado negativo: risco para doenças e impactos na saúde, criminalidade, acidentes
+### de trânsito, e para alguns, dependência. Globalmente, o consumo de álcool causa
+### 2,8 milhões de mortes prematuras por ano.
+
+### Esse registro mostra os dados sobre padrões globais do consumo de álcool, padrões
+### de consumo, tipos de bebidas, prevalência de alcolismo; e as consequências do consumo,
+### incluindo crime, mortalidade e acidentes de trânsito.
+
+# Carregar pacotes -------------------------------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+library(cols4all)
+library(hrbrthemes)
+
+# Carregar dados ---------------------------------------------------------------------------------------------------------------------------
+
+
+
+
