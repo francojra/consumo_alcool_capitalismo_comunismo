bf811c58f17ea82d02dcb838ed56c737af18a346
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-06 00:01:43 -0300

    Add files via upload

--- consumo_alcool_capitalismo_comunismo.R
+++ consumo_alcool_capitalismo_comunismo.R
@@ -47,3 +47,23 @@ cons_alc1 <- cons_alc %>%
             se = sd/sqrt(n)) %>%
   view()
 
+# Gráficos ---------------------------------------------------------------------------------------------------------------------------------
+
+c4a("safe", 6)
+
+ggplot(cons_alc1, aes(x = fct_reorder(Entity, media), y = media, 
+                      fill = Entity)) +
+  geom_col(width = 0.9) +
+  geom_errorbar(aes(ymin = media - se, ymax = media + se),
+                size = 0.8, width = 0.2) +
+  scale_fill_manual(values = c("#88CCEE", "#CC6677",
+                               "#DDCC77", "#117733",
+                               "#332288", "#AA4499")) +
+  scale_y_continuous(expand = expansion(mult = c(0,0))) +
+  scale_x_discrete(labels = c("Coreia do Norte", "Cuba", "China",
+                              "Japão", "Estados Unidos", "Alemanha")) +
+  labs(x = "Países", y = "Consumo de álcool por pessoa (litros)") +
+  theme_ipsum(axis_title_size = 16,
+              axis_text_size = 14) +
+  theme(legend.position = "none",
+        axis.text = element_text(color = "black"))
